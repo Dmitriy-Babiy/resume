789ac6986e55ddc83688f84482d54db412cee9ef
Dmitriy Babiy <dimascelecto@gmail.com>
2023-12-23 03:12:58 +0700

    fix: build

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import App from './app.tsx';
 import './assets/styles/index.scss';
+import App from './app';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
